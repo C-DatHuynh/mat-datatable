c53ff795c2697a822c349a5888477fb04be1a007
Cong Dat Huynh <dat.huynhcong95@gmail.com>
2025-06-09 18:34:39 +0200

    Add common files

---
+++ src/app/types.ts
@@ -0,0 +1,115 @@
+import { ValidatorFn } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+export interface ColumnDefinition {
+  name: string;
+  label?: string;
+  options: ColumnOptions;
+}
+
+export type PrimitiveType = string | number | boolean | null | undefined;
+export interface SelectOptionType {
+  key: string;
+  value: Exclude<PrimitiveType, null | undefined>;
+}
+export type FormControlType = 'textbox' | 'dropdown' | 'multiselect' | 'checkbox' | 'slider';
+
+export interface ColumnSubOptions {
+  order?: number; // Order of the filter in the UI
+  fullWidth?: boolean; // Full width for dropdown or multiselect
+  renderLabel?: (value: PrimitiveType) => string; // Function to render label in the filter dropdown or multiselect
+  logic?: (itemValue: PrimitiveType, filterValue: PrimitiveType | PrimitiveType[], row: object) => boolean; // Custom logic for filtering
+  selectOptions?: SelectOptionType[] | Observable<SelectOptionType[]>;
+  controlType?: FormControlType;
+}
+
+export interface ColumnOptions {
+  display?: boolean;
+  editable?: boolean;
+  filter?: boolean;
+  filterOptions?: ColumnSubOptions;
+  editOptions?: ColumnSubOptions;
+  sort?: boolean;
+  validators?: ValidatorFn | ValidatorFn[] | ((value: PrimitiveType, row: object) => boolean);
+}
+
+export interface Action {
+  label?: string;
+  icon?: string;
+  color?: string;
+  onClick: () => void;
+  disabled?: () => boolean;
+}
+
+export interface RowAction {
+  label?: string;
+  icon?: string;
+  onClick: (data: object, rowIndex: number) => void;
+  disabled?: (data: object, rowIndex: number) => boolean;
+}
+
+export interface TableOptions {
+  rowsPerPage?: number;
+  rowsPerPageOptions?: number[];
+  customActions?: Action[]; // Add, Print, Export, Redirect
+  customRowActions?: RowAction[]; // Edit, Delete
+  jumpToPage?: boolean;
+  selectableRows?: 'none' | 'single' | 'multiple';
+  expandableRows?: boolean; // Allow row expansion
+  //selectableRowsHeader?: boolean; // Show checkbox in header
+  //selectableRowsOnClick?: boolean; // Allow row selection on click
+  search?: boolean; // Enable search functionality
+  searchPlaceholder?: string;
+  textLabels?: TextLabels; // Localization
+  canAdd?: boolean; // Show Add button
+  canPrint?: boolean; // Show Print button
+  canExport?: boolean; // Show Export button
+  canEdit?: boolean; // Show Edit button
+  canDelete?: boolean; // Show Delete button
+  canFilter?: boolean; // Show Filter button
+}
+
+export interface TextLabels {
+  body?: {
+    nomatch?: string;
+    toolTip?: string;
+  };
+  pagination?: {
+    next?: string;
+    previous?: string;
+    rowsPerPage?: string;
+    displayRows?: string;
+    jumpToPage?: string;
+  };
+  toolbar?: {
+    search?: string;
+    downloadCsv?: string;
+    print?: string;
+    viewColumns?: string;
+    filterTable?: string;
+  };
+  filter?: {
+    all?: string;
+    title?: string;
+    reset?: string;
+  };
+  viewColumns?: {
+    title?: string;
+    titleAria?: string;
+  };
+  selectedRows?: {
+    text?: string;
+    delete?: string;
+    deleteAria?: string;
+  };
+}
+
+export interface DataModel {
+  id: number;
+  name: string;
+}
+
+export interface FilterPayload {
+  textSearch?: string;
+  formSearch?: Record<string, PrimitiveType | PrimitiveType[]>;
+}
