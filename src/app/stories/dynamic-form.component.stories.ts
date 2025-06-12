64de003d2aa22a7198a30453a4781a836b9d62fe
Cong Dat Huynh <dat.huynhcong95@gmail.com>
2025-06-12 22:37:52 +0200

    Update stories

--- src/app/stories/dynamic-form.component.stories.ts
+++ src/app/stories/dynamic-form.component.stories.ts
@@ -2,7 +2,6 @@
 import { Validators } from '@angular/forms';
 import type { Meta, StoryObj } from '@storybook/angular';
 import { DynamicFormComponent } from '../dynamic-form';
-import { CheckboxFormControl, MultiSelectFormControl, SelectFormControl, SliderFormControl, TextboxFormControl } from '../dynamic-form';
 
 const meta: Meta<DynamicFormComponent> = {
   title: 'Dynamic Form',
@@ -15,22 +14,27 @@ type Story = StoryObj<DynamicFormComponent>;
 
 export const Default: Story = {
   args: {
-    controls: [
-      new TextboxFormControl({ key: 'name', label: 'Name', value: '' }),
-      new SelectFormControl({
-        key: 'gender',
+    formId: 'dynamic-form',
+    formValue: {
+      name: '',
+      gender: 'male',
+      letters: ['A', 'B'],
+      active: true,
+      age: 30,
+    },
+    controlOptions: {
+      name: { label: 'Name', controlType: 'textbox', type: 'text' },
+      gender: {
         label: 'Gender',
-        value: 'male',
         selectOptions: [
           { key: 'Male', value: 'male' },
           { key: 'Female', value: 'female' },
         ],
+        controlType: 'dropdown',
         validators: Validators.required,
-      }),
-      new MultiSelectFormControl({
-        key: 'letters',
+      },
+      letters: {
         label: 'Letters',
-        value: ['A', 'B'],
         selectOptions: [
           { key: 'A', value: 'A' },
           { key: 'B', value: 'B' },
@@ -39,20 +43,20 @@ export const Default: Story = {
           { key: 'E', value: 'E' },
           { key: 'F', value: 'F' },
         ],
-      }),
-      new CheckboxFormControl({
-        key: 'active',
+        controlType: 'multiselect',
+      },
+      active: {
         label: 'Is Active',
         value: true,
-      }),
-      new SliderFormControl({
-        key: 'age',
+        controlType: 'checkbox',
+      },
+      age: {
         label: 'Age',
-        value: 30,
         min: 0,
         max: 50,
         step: 1,
-      }),
-    ],
+        controlType: 'slider',
+      },
+    },
   },
 };
--- src/app/stories/mui-datatable.component.stories.ts
+++ src/app/stories/mui-datatable.component.stories.ts
@@ -12,6 +12,8 @@ const meta: Meta<DataTableComponent<PeriodicElement>> = {
 export default meta;
 type Story = StoryObj<DataTableComponent<PeriodicElement>>;
 
+const selectOptions = ELEMENT_DATA.map(item => ({ key: item.symbol, value: item.symbol }));
+
 export const Default: Story = {
   args: {
     title: 'Example Basic Material Datatable',
@@ -23,7 +25,11 @@ export const Default: Story = {
       { name: 'name', label: 'Name', options: { display: true, filter: true, sort: true } },
       { name: 'position', label: 'Position', options: { display: true, filter: true, sort: true } },
       { name: 'weight', label: 'Weight', options: { display: true, filter: true, sort: true } },
-      { name: 'symbol', label: 'Symbol', options: { display: true, filter: true, sort: false, filterOptions: { controlType: 'dropdown' } } },
+      {
+        name: 'symbol',
+        label: 'Symbol',
+        options: { display: true, filter: true, sort: false, filterOptions: { controlType: 'dropdown', selectOptions } },
+      },
       { name: 'description', label: 'Description', options: { display: false, filter: false, sort: false } },
     ],
   },
