eea01a7894616af303494db6237271e07fb31dac
Cong Dat Huynh <dat.huynhcong95@gmail.com>
2025-12-01 00:25:03 +0100

    Correct logic of AddEditDialog

--- projects/material-datatable/src/lib/mui-datatable/mui-datatable.component.ts
+++ projects/material-datatable/src/lib/mui-datatable/mui-datatable.component.ts
@@ -372,11 +372,11 @@ export abstract class DataTableComponent<TModel> implements AfterViewInit {
       title: isEdit ? `Edit item` : 'Add item',
       actionLabel: isEdit ? 'Save' : 'Add',
       onResult: ({ action, data }) => {
-        const selectedIndex = this.selectedIndex();
-        if (action.type !== 'ok' || selectedIndex === null) {
+        if (action.type !== 'ok') {
           return;
         }
-        if (isEdit) {
+        const selectedIndex = this.selectedIndex();
+        if (isEdit && selectedIndex !== null) {
           this.updateItem(selectedIndex, data);
         } else if (!isEdit) {
           this.addItem(data);
