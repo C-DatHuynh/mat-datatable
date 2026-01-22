8721b59ea68746363313235b534d7d16281288ff
Cong Dat Huynh <dat.huynhcong95@gmail.com>
2026-01-22 03:23:50 +0100

    Correct laod remote logic

--- projects/material-datatable/src/lib/mui-datatable/remote-datatable.component.ts
+++ projects/material-datatable/src/lib/mui-datatable/remote-datatable.component.ts
@@ -72,12 +72,12 @@ export class RemoteDataTableComponent<TModel> extends DataTableComponent<TModel>
   }
 
   loadRemoteData(): void {
-    if (!this.dataStoreService.settings()?.table.remote) {
-      return;
-    }
     const pagination = this.dataStoreService.pagination();
     const sorting = this.dataStoreService.sorting();
     const filter = this.dataStoreService.filters();
-    this.remoteDataTableService.populateItems(pagination, filter, sorting);
+    const settings = this.dataStoreService.settings();
+    if (settings?.table.remote) {
+      this.remoteDataTableService.populateItems(pagination, filter, sorting);
+    }
   }
 }
